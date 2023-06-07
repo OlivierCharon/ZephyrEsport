cdf796208f4c74b59d39ec3f62c807b1e0a436e8
Olivier CHARON <ochar@P-LYO-BUNNY.home>
2023-06-07 19:08:15 +0200

    Current issue: how to set a placeholder/default image when the image file does not exist with Nuxt3?

--- components/News/Articles.vue
+++ components/News/Articles.vue
@@ -1,11 +1,29 @@
 <template>
     <div class="mx-44">
-        <NewsArticle v-for="i in 5" :key="i" :article="{'id':i,'kind':'kind'+i}"/>
+        <Suspense>
+            <template #default>
+                <NewsArticle v-for="article in articles.data" :key="article.id" :article="article.id"/>
+            </template>
+            <template #fallback>
+                LOading...
+            </template>
+        </Suspense>
     </div>
 </template>
 
 <script setup>
-
+    const { data:articles, pending, error, refresh } = await useFetch('http://127.0.0.1:8000/api/posts'
+    // ,{
+    //     query: { 
+    //         perPage: null,
+    //         page: null, 
+    //         title: null,
+    //         txt: null
+    //     }
+    // }
+    )
+    // console.log(data)
+    console.log(articles)
 </script>
 
 <style>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ['@nuxtjs/tailwindcss'],
+    render: { 
+        fallback: false 
+    }
 })
+
