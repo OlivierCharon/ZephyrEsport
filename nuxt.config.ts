8bea8c59143d2d9e346ea0c0a43f012dff238cbd
Olivier CHARON <olivier.charon@outlook.fr>
2023-09-21 13:52:26 +0200

    MAJ du gitignore et config gitsecret

---
+++ nuxt.config.ts
@@ -0,0 +1,45 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "@nuxt/image-edge",
+        "dayjs-nuxt",
+        [
+            "@pinia/nuxt",
+            {
+                autoImports: ["defineStore", "acceptHMRUpdate", "storeToRefs"],
+            },
+        ],
+    ],
+    css: ["~/assets/css/main.css"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
+    dayjs: {
+        locales: ["en", "fr"],
+        plugins: ["relativeTime", "utc", "timezone"],
+        defaultLocale: "en",
+        defaultTimezone: "America/New_York",
+    },
+    runtimeConfig: {
+        public: {
+            API_BASE_URL: process.env.API_BASE_URL,
+        },
+    },
+    image: {
+        provider: "imagekit",
+        imagekit: {
+            baseURL: process.env.IMAGE_KIT_URL,
+        },
+    },
+    imports: {
+        dirs: ["stores"],
+    },
+    build: {
+        // vue-toastification - old commonjs module
+        transpile: ["vue-toastification"],
+    },
+});
