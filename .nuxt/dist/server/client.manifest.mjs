f3d0f96568d6b35c51794a6c08432a5e5f39a4b9
Olivier CHARON <olivier.charon@outlook.fr>
2023-10-19 16:49:27 +0200

    Removing .nuxt files

--- .nuxt/dist/server/client.manifest.mjs
+++
@@ -1,1 +0,0 @@
-export { default } from "file:///home/ochar/Perso/ZephyrEsport/node_modules/@nuxt/vite-builder/dist/runtime/client.manifest.mjs"
--- .nuxt/types/layouts.d.ts
+++
@@ -1,7 +0,0 @@
-import { ComputedRef, Ref } from 'vue'
-export type LayoutKey = "admin" | "default" | "shop"
-declare module "/home/ochar/Perso/ZephyrEsport/node_modules/nuxt/dist/pages/runtime/composables" {
-  interface PageMeta {
-    layout?: false | LayoutKey | Ref<LayoutKey> | ComputedRef<LayoutKey>
-  }
-}
