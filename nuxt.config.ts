9b5764b724e4f50bd23fca7acd4c455e6b65f266
OlivierCharon <olivier.charon@outlook.fr>
2023-11-29 12:36:44 +0100

    fixing package.json file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
     modules: [
         "@nuxtjs/tailwindcss",
-        "@nuxt/image-edge",
+        "@nuxt/image",
         "dayjs-nuxt",
         [
             "@pinia/nuxt",
